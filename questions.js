746ae779bfe300e3c108548bc4647595d6b9b836
Ahad009 <97398948+Ahad009@users.noreply.github.com>
2024-01-22 12:37:23 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for in web development?",
+        answer: "A) HyperText Markup Language",
+        options: [
+            "A) HyperText Markup Language",
+            "B) High-Level Text Management",
+            "C) Hypertext Model Language",
+            "D) Hyperlink and Text Manipulation"
+        ]
+    },
+    {
+        numb: 2,
+        question: "In programming, what does IDE stand for?",
+        answer: "A) Integrated Development Environment ",
+        options: [
+            "A) Integrated Development Environment ",
+            "B) Interactive Design Element",
+            "C) Interface Design Engine",
+            "D) Intelligent Development Execution"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "What is the purpose of SQL in database management?",
+        answer: "A) Structured Query Language",
+        options: [
+            "A) Structured Query Language",
+            "B) Simple Query Logic",
+            "C) System Query Link",
+            "D) Standardized Question Language",
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does CSS stand for in web design?",
+        answer: "A) Cascading Style Sheets",
+        options: [
+            "A) Cascading Style Sheets",
+            "B) Computer Style System",
+            "C) Creative Style Set",
+            "D) Content Structure Sheet",
+        ]
+    },
+    {
+        numb: 5,
+        question: "In networking, what does TCP/IP stand for?",
+        answer: "A) Transmission Control Protocol/Internet Protocol",
+        options: [
+            "A) Transmission Control Protocol/Internet Protocol",
+            "B) Technical Communication Port/Input Port",
+            "C) Time-Critical Protocol/Information Protocol",
+            "D) Total Connection Path/Internet Protocol",
+        ]
+    }
+];
